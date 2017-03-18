fcab90d3e491793f70f11b14f5f2ef21e1f24ba1
Kevin Tong <kcr1230@hotmail.com>
2017-03-18 15:28:36 -0700

    Yiruma.ogg => yiruma.ogg

--- testing/VideoSync/src/tutorial/Tutorial.java
+++ testing/VideoSync/src/tutorial/Tutorial.java
@@ -1,42 +1,29 @@
 package tutorial;
 
 import java.awt.BorderLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
-
 import uk.co.caprica.vlcj.component.EmbeddedMediaPlayerComponent;
 import uk.co.caprica.vlcj.discovery.NativeDiscovery;
 
 public class Tutorial {
 
   private final JFrame frame;
-
   private final EmbeddedMediaPlayerComponent mediaPlayerComponent;
-
   private final JButton pauseButton;
-
   private final JButton rewindButton;
-
   private final JButton skipButton;
 
   public static void main(final String[] args) {
     new NativeDiscovery().discover();
-    SwingUtilities.invokeLater(new Runnable() {
-      @Override
-      public void run() {
-        new Tutorial(args);
-      }
-    });
+    SwingUtilities.invokeLater(Tutorial::new);
   }
 
-  public Tutorial(String[] args) {
+  private Tutorial() {
     frame = new JFrame("Video Sync");
     frame.setBounds(100, 100, 600, 400);
     frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
@@ -63,29 +50,14 @@ public class Tutorial {
     controlsPane.add(skipButton);
     contentPane.add(controlsPane, BorderLayout.SOUTH);
 
-    pauseButton.addActionListener(new ActionListener() {
-      @Override
-      public void actionPerformed(ActionEvent e) {
-        mediaPlayerComponent.getMediaPlayer().pause();
-      }
-    });
+    pauseButton.addActionListener(e -> mediaPlayerComponent.getMediaPlayer().pause());
 
-    rewindButton.addActionListener(new ActionListener() {
-      @Override
-      public void actionPerformed(ActionEvent e) {
-        mediaPlayerComponent.getMediaPlayer().skip(-10000);
-      }
-    });
+    rewindButton.addActionListener(e -> mediaPlayerComponent.getMediaPlayer().skip(-10000));
 
-    skipButton.addActionListener(new ActionListener() {
-      @Override
-      public void actionPerformed(ActionEvent e) {
-        mediaPlayerComponent.getMediaPlayer().skip(10000);
-      }
-    });
+    skipButton.addActionListener(e -> mediaPlayerComponent.getMediaPlayer().skip(10000));
 
     frame.setContentPane(contentPane);
     frame.setVisible(true);
-    mediaPlayerComponent.getMediaPlayer().playMedia("src/tutorial/Yiruma.ogg");
+    mediaPlayerComponent.getMediaPlayer().playMedia("tutorial/yiruma.ogg");
   }
 }
