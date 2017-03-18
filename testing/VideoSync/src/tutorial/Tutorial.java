5465658d951e04630bc68744797c6ccc63b4011b
Kevin Tong <kcr1230@hotmail.com>
2017-03-18 15:28:36 -0700

    Revert "Yiruma.ogg => yiruma.ogg"
    This reverts commit 045ca3fcd2eba84b0d37f96f4b01682c6ae91f07.

--- testing/VideoSync/src/tutorial/Tutorial.java
+++ testing/VideoSync/src/tutorial/Tutorial.java
@@ -1,29 +1,42 @@
 package tutorial;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
+
 import uk.co.caprica.vlcj.component.EmbeddedMediaPlayerComponent;
 import uk.co.caprica.vlcj.discovery.NativeDiscovery;
 
 public class Tutorial {
 
   private final JFrame frame;
+
   private final EmbeddedMediaPlayerComponent mediaPlayerComponent;
+
   private final JButton pauseButton;
+
   private final JButton rewindButton;
+
   private final JButton skipButton;
 
   public static void main(final String[] args) {
     new NativeDiscovery().discover();
-    SwingUtilities.invokeLater(Tutorial::new);
+    SwingUtilities.invokeLater(new Runnable() {
+      @Override
+      public void run() {
+        new Tutorial(args);
+      }
+    });
   }
 
-  private Tutorial() {
+  public Tutorial(String[] args) {
     frame = new JFrame("Video Sync");
     frame.setBounds(100, 100, 600, 400);
     frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
@@ -50,14 +63,29 @@ public class Tutorial {
     controlsPane.add(skipButton);
     contentPane.add(controlsPane, BorderLayout.SOUTH);
 
-    pauseButton.addActionListener(e -> mediaPlayerComponent.getMediaPlayer().pause());
+    pauseButton.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        mediaPlayerComponent.getMediaPlayer().pause();
+      }
+    });
 
-    rewindButton.addActionListener(e -> mediaPlayerComponent.getMediaPlayer().skip(-10000));
+    rewindButton.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        mediaPlayerComponent.getMediaPlayer().skip(-10000);
+      }
+    });
 
-    skipButton.addActionListener(e -> mediaPlayerComponent.getMediaPlayer().skip(10000));
+    skipButton.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        mediaPlayerComponent.getMediaPlayer().skip(10000);
+      }
+    });
 
     frame.setContentPane(contentPane);
     frame.setVisible(true);
-    mediaPlayerComponent.getMediaPlayer().playMedia("tutorial/yiruma.ogg");
+    mediaPlayerComponent.getMediaPlayer().playMedia("src/tutorial/Yiruma.ogg");
   }
 }
