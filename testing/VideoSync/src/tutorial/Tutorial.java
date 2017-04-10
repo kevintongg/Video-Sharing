e74b49e76a5551ab7e651c305cec90fe1097ac0a
Daniel Kale <dkalechip@gmail.com>
2017-04-10 13:41:09 -0700

    Partial implementation of volume bar

--- testing/VideoSync/src/tutorial/Tutorial.java
+++ testing/VideoSync/src/tutorial/Tutorial.java
@@ -1,6 +1,7 @@
 package tutorial;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
@@ -12,15 +13,24 @@ import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.JSlider;
 import javax.swing.KeyStroke;
 import javax.swing.SwingUtilities;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+import uk.co.caprica.vlcj.binding.LibVlcConst;
 import uk.co.caprica.vlcj.component.EmbeddedMediaPlayerComponent;
 import uk.co.caprica.vlcj.discovery.NativeDiscovery;
+import uk.co.caprica.vlcj.player.Equalizer;
+import uk.co.caprica.vlcj.player.MediaPlayer;
+import uk.co.caprica.vlcj.player.MediaPlayerEventAdapter;
+import uk.co.caprica.vlcj.player.MediaPlayerFactory;
 
 public class Tutorial {
-
+	
   private final EmbeddedMediaPlayerComponent mediaPlayerComponent;
-
+  private JSlider volumeSlider;
   public static void main(final String[] args) {
     new NativeDiscovery().discover();
     SwingUtilities.invokeLater(Tutorial::new);
@@ -75,12 +85,26 @@ public class Tutorial {
     JButton playButton = new JButton("Play");
     JButton rewindButton = new JButton("Rewind"); // 5 seconds backwards
     JButton forwardButton = new JButton("Forward"); // 5 seconds forward
+    
+    volumeSlider = new JSlider();
+    volumeSlider.setOrientation(JSlider.HORIZONTAL);
+    volumeSlider.setMinimum(LibVlcConst.MIN_VOLUME);
+    volumeSlider.setMaximum(LibVlcConst.MAX_VOLUME);
+    volumeSlider.setPreferredSize(new Dimension(100, 40));
+    volumeSlider.setToolTipText("Change volume");
+    
+    mediaPlayerComponent.addComponentListener(new MediaPlayerEventAdapter() {
+        @Override
+        public void playing(MediaPlayer mediaPlayer) {
+        }
+    });
 
     controlsPane.add(playButton);
     controlsPane.add(pauseButton);
     controlsPane.add(rewindButton);
     
     controlsPane.add(forwardButton);
+    controlsPane.add(volumeSlider);
 
     contentPane.add(controlsPane, BorderLayout.SOUTH);
 
@@ -91,9 +115,22 @@ public class Tutorial {
     rewindButton.addActionListener(e -> mediaPlayerComponent.getMediaPlayer().skip(-5000));
 
     forwardButton.addActionListener(e -> mediaPlayerComponent.getMediaPlayer().skip(5000));
+    
+    volumeSlider.addChangeListener(new ChangeListener() {
+        @Override
+        public void stateChanged(ChangeEvent e) {
+            JSlider source = (JSlider)e.getSource();
+           mediaPlayerComponent.setVolume(source.getValue());
+        }
+    });
+    
 
     frame.setJMenuBar(createMenuBar());
     frame.setContentPane(contentPane);
     frame.setVisible(true);
   }
+  private void setVolume(int value) {
+      volumeSlider.setValue(value);
+  }
+
 }
